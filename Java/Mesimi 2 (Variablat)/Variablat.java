efe77f148b536d5824b00c16c439791384a8b184
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-04-26 20:34:40 +0200

    Variablat

---
+++ Java/Mesimi 2 (Variablat)/Variablat.java
@@ -0,0 +1,84 @@
+
+public class Variablat {
+	 public static void main (String[] args) {
+		 //String emri = "Alpet"; // Variabel vetem per text, karaktere
+		 //System.out.println(emri);
+		 
+		 
+		 String Shkolladigjitale; //Deklarimi i variabes
+		 //Shkolladigjitale = "Alpet Gexha";//mbushje e saj
+		 //System.out.println(Shkolladigjitale);
+		 
+		 Shkolladigjitale = "Alpet Gexha";
+		 System.out.println(Shkolladigjitale);
+		 
+		 Shkolladigjitale = "Shkolladigjitale";
+		 System.out.println(Shkolladigjitale);
+		 
+		 
+		 
+//		 byte bytenumber = 100; // vetem numrat -128 deri 127
+//		 System.out.println(bytenumber);
+		 
+		 
+//		 short shortnumer = 3000; // 32768
+//		 System.out.println(shortnumer);
+		 
+		 
+//		 int intnumber = 59012;  // -9B deri 9B
+//		 System.out.println(intnumber);
+		 
+		 
+//		 long longnumber = 93021391;
+//		 System.out.println(longnumber);
+		 
+		 
+//		 float floatnumber = 3.32f; // numrat me presje 
+//		 System.out.println(floatnumber);
+		 
+		 
+		 
+//		 double doublenumber = 32,31; // numrat me presje
+//		 System.out.println(doublenumber);
+		 
+		 
+//		 char charkarakteret = 'A'; // vetem per 1 karakter
+//		 System.out.println(charkarakteret);
+		 
+		 
+		 //Bashkimi i 2 stringave
+//		 String dita = "E hene";
+//		 System.out.println("Sot eshte dita" + dita);
+		 
+//		 String school = "Shkolla" + " " + "Digjitale";
+//		 System.out.println(school + " 2020");
+		 
+		 
+//		 String s1 = "Shkolla";
+//		 String s2 = " Digjitale";
+//		 String s3 = s1.concat(s2); // concat bashkimi i stringave
+//		 System.out.println(s3);
+		 
+		 
+		 //convertimi i variablave automatike
+//		 int numri = 10;
+//		 double numri2 = numri;
+//		 System.out.println(numri);
+//		 System.out.println(numri2);
+		 
+		 
+		 
+//		 double numri1 = 10.8;
+//		 int numri2 = (int)numri1;
+//		 System.out.println(numri1);
+//		 System.out.println(numri2);
+		 
+		 
+		 
+		 
+		 
+		 
+	 }
+} 
+  
+
