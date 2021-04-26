fb27eaa4f7958a6571924034d5251f6b282759e2
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-04-26 20:28:50 +0200

    encrypted password

---
+++ PHP/encrypted password/encrypted.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+</head>
+
+<body>
+	<?php
+	$password = 'alpetgexha';
+	$password_hash = mp5($password);
+	echo $password_hash;
+	?>
+</body>
+
+</html>
