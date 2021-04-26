3f121b0473725a20f3db6e791fed89fddb6c99ba
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-04-26 20:34:23 +0200

    Hello World

---
+++ Java/Mesimi  1 (Hello World)/projekti_1.java
@@ -0,0 +1,19 @@
+/*Perdoret per
+Mobile applications (specially Android apps)
+Desktop applications
+Web applications
+Web servers
+and application
+servers
+Games
+Database connection
+*/
+
+package shkolladigjitaleag;
+
+public class projekti_1 { //clasa duhet te jete e njet me emrin e failit
+	public static void main(String[] args) { //args argumenti 
+		System.out.println("Hello World"); //shkrimi
+	}
+}
+
