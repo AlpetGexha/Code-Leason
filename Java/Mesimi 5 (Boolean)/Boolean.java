d7f7e40170d3644d955407f24f69b71b365731d7
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 15:57:22 +0200

    Boolean

---
+++ Java/Mesimi 5 (Boolean)/Boolean.java
@@ -0,0 +1,46 @@
+package Boolean;
+import java.util.Scanner;
+//True & False
+public class Boolean {
+
+	public static void main(String[] args) {
+		//int a = 11;
+		//int b = 18;
+		
+
+		
+		
+//		System.out.println(a<b);
+//		System.out.println(a>b);
+//		System.out.println(a==b);
+		
+		
+		
+//		if(a<b) { System.out.println("Vlera a eshte me e vogel se vlera e b-s"); }
+//		if(a>b) { System.out.println("Vlera a eshte me e madhe se vlera e b-s"); }
+//		if(a==b) { System.out.println("Vlerat jane ta barabarta"); }
+		
+		
+		
+//		if(a<b) { System.out.println("Vlera a eshte me e vogel se vlera e b-s"); }
+//		else if (a>b) { System.out.println("Vlera a eshte me e madhe se vlera e b-s"); }
+//		else { System.out.println("Vlerat jane ta barabarta");}
+		
+		
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Shruani Numrin: ");
+		int numri = s.nextInt();
+		
+				
+		if(numri % 2 == 0 )  { System.out.println("Numer Qift"); }
+		else { System.out.println("Numer Tek"); }
+	
+		
+		
+		
+	
+	}
+
+
+}
