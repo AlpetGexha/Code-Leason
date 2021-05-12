f02160d509b61fcc961c8b0cd7d7db463753c8d5
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 23:22:43 +0200

    switch

---
+++ Java/Mesimi 6 (Operatort Switch)/switch1.java
@@ -0,0 +1,47 @@
+package operatort_switch;
+
+public class switch1 {
+	  public static void main (String[] args) {
+		  
+		     	int day = 1;
+//		  		switch(day) {
+//		  		case 1:
+//		  			System.out.println("Dita e Henë");
+//		  			break;
+//		  		case 2:
+//		  			System.out.println("Dita e Mart");
+//		  			break;
+//		  		case 3:
+//		  			System.out.println("Dita e Merkur");
+//		  			break;
+//		  		case 4:
+//		  			System.out.println("Dita e Enjete");
+//		  			break;
+//		  		case 5:
+//		  			System.out.println("Dita e Premte");
+//		  			break;
+//		  		case 6:
+//		  			System.out.println("Dita e Shtune");
+//		  			break;
+//		  		case 7:
+//		  			System.out.println("Dita e Dielle");
+//		  			break;
+//		  			
+//		  		}
+		  				  		
+		  		switch (day) {
+		  		case 6 :
+		  			System.out.println("e Shtune");
+		  			break;
+		  		case 7 : 
+		  			System.out.println("e Diel");
+		  			break;
+		  			
+		  			default: 
+		  				System.out.println("Dite pune");
+		  		
+		  		}
+		  		
+
+	  }
+}
