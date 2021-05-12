10c4d1cdb8e3c66a18184eb776361b2f538ab344
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 23:16:26 +0200

    Operatort

---
+++ Java/Mesimi 6 (Operatort Switch)/operatort.java
@@ -0,0 +1,43 @@
+package operatort_switch;
+
+public class operatort {
+	public static void main (String[] args) {
+		int age = 16;
+		int leght = 165; 
+		// !-negativi -e kunderta
+//		System.out.println(!(age > 14));
+//		System.out.println(!(leght == 165));
+		
+		/*
+		  &&-dhe 
+		  nese njera edhe FALES Resulti eshe FALSE
+		  
+		 */
+		
+//		System.out.println((age > 15) && (leght < 150));// True && False
+//		System.out.println((age > 15) && (leght > 150));// True && True
+//		System.out.println((age < 15) && (leght > 150));// False && True
+		
+		/*
+		 || - ose s
+		 nese njera eshte TRUE resultati eshte TRUE
+		 
+		 */
+			 	
+//		System.out.println((age > 15) || (leght < 150));// True && False
+//		System.out.println((age > 15) || (leght > 150));// True && True
+//		System.out.println((age < 15) || (leght > 150));// False && True
+//		System.out.println((age < 15) || (leght < 150));// False && False
+		
+/*
+ ^ - vetem njera duhet te jete True dhe tjetra False perndryhse rezultati eshte false
+ */
+				
+//		System.out.println((age > 15) ^ (leght < 150));// True && False
+//		System.out.println((age > 15) ^ (leght > 150));// True && True
+//		System.out.println((age < 15) ^ (leght > 150));// False && True
+//		System.out.println((age < 15) ^ (leght < 150));// False && False
+		
+	
+	}
+}
