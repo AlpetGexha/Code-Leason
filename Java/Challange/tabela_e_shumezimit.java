6298c042c221b0bbaafbfe50319f87f35af70e89
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 23:45:32 +0200

    tabela_e_shumezimit
    for in for

---
+++ Java/Challange/tabela_e_shumezimit.java
@@ -0,0 +1,36 @@
+package Challage;
+
+public class tabela_e_shumezimit {
+	public static void main (String[] args) {
+//		for (int i=0; i<=10; i++) {
+//			for (int j=0; j<=10; j++) {
+//				System.out.println(i + "* "+ j + " = " + (i*j));
+//			}
+//		}
+//	}
+	
+		
+		
+//		int mol = 1;
+//		int i = 1;
+		
+//		while(mol <= 10) {
+//			mol*=i;
+//			i++;
+//		}
+//		System.out.println(mol);
+//		
+		
+		
+		
+//		for (int i=1; i<=10; i++) {
+//			mol*=i;
+//		}
+//		System.out.println(mol);
+//		
+		
+		s
+		
+		
+	}
+}
