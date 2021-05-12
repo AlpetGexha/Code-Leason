93bc955cf609d6cb6482d382a8c9d8e90ec7dbea
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 23:45:11 +0200

    siperfaqja
    operatort

---
+++ Java/Challange/siperfaqja.java
@@ -0,0 +1,27 @@
+package Challage;
+
+public class siperfaqja {
+
+	public static void main(String[] args) {
+//		int a = 15;
+//		int b = 11;
+//		
+//		System.out.println("Siperfaqja e drejtkendeshit me brinjet a="+a+" dhe b="+b+" eshte " + a*b);
+		
+		
+		
+		int a = 10;
+		int b = 15;
+		int c = 20;
+		
+		System.out.println(Math.min(Math.min(a, b),c));
+		
+		if((a>b) && (a>b)) {System.out.println(a);}
+		if((b>a) && (b>c)) {System.out.println(b);}
+		else {System.out.println(c);}
+	
+	
+}
+
+
+}
