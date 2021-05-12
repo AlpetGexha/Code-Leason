bac6540b1925c3fdaf1f462c5715961344e1e60a
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-12 23:27:46 +0200

    while

---
+++ Java/Mesimi 7 (For While)/while1.java
@@ -0,0 +1,40 @@
+package mesim7;
+
+public class while1 {
+
+	public static void main(String[] args) {
+		
+		int count = 1;
+		
+		
+//		while (count < 6) {
+//			System.out.println(count + ".Shkolla Digjitele");
+//			count++;
+//		}
+		
+		
+		
+		// Ne fillim beje funkison pastaj shiko whikins
+//		do {
+//			System.out.println(count + ".Shkolla Digjitale");
+//			count++;
+//		} while (count < 6);
+		
+		
+		
+		int sum = 0;
+		int i = 0;
+		
+		
+		while(i<=100) {
+			sum+=
+			i++;
+		}
+		System.out.println("Shuma në mes 0-100 është " + sum);
+		
+	}
+	
+	
+	
+
+}
