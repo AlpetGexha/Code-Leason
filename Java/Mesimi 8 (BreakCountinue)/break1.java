fcbf3edc4e2b920b3a6c2065486a986cb3504f7e
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-17 20:53:24 +0200

    Break

---
+++ Java/Mesimi 8 (BreakCountinue)/break1.java
@@ -0,0 +1,22 @@
+package BreakCountinue;
+
+public class break1 {
+
+	public static void main(String[] args) {
+		// Break nes kryet funksioni ndalo funskionin kalo te tjetri
+		
+		
+	for(int i = 0; i<10; i++) {
+		if(i == 4) { break; } //kur numri shkon deri ne 3 ndalo funksionin
+		System.out.print(i); 
+		
+		//nese numir nuk eshe = 3 ndalo presjen
+		if(i!=3) {
+			System.out.print(",");
+		}
+		
+		}	
+
+	}
+
+}
