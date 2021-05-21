9a31d04995cffcd020bb62ed3dc1a7644ccf8a9c
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-05-21 20:08:41 +0200

    klasa, objekti, atributet e klasës, konstantat , konstruktori, destruktori

--- PHP/OOP/1.klasa, objekti, atributet e klasës, konstantat , konstruktori, destruktori.php
+++ PHP/OOP/1.klasa, objekti, atributet e klasës, konstantat , konstruktori, destruktori.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 /*       
         klasa, objekti, atributet e klasës, konstantat , konstruktori, destruktori
 */
@@ -69,50 +67,3 @@ echo "<br>";
 $Rrethi->r = 20; //per ndryshimin e vleres(Vetem ne klasa publike)
 //funkisoni i perimetirt
 echo "Perimetri: " . $Rrethi->perimetri();
-
-
-
-
-/*    
-    definimi i tipeve, setter/getters
-*/
-class Studenti
-{
-    private $emri, $mbiemri, $mosha;
-
-    public function __construct($emri, $mbiemri, $mosha = 18)
-    {
-        $this->emri = $emri;
-        $this->mbiemri = $mbiemri;
-        $this->mosha = $mosha;
-    }
-    // __toString Funskion me vlere kthyse ("return") qe kethen vetem String
-    public function __toString()
-    {
-        return "Ermi: " . $this->emri . " Mbiemri: " . $this->mbiemri . " Mosha: " . $this->mosha . "<br>";
-    }
-
-    // e ndan vleren 
-    public function setName($emri)
-    { //:void
-        $this->emri = $emri;
-    }
-    //e e merr vlern
-    public function getName()
-    { //:string vlere kthyse
-        return $this->emri;
-    }
-}
-
-
-
-$Alpet = new Studenti("Alpet", "Gexha", 16);
-$Filan = new  Studenti("Filan", "Fisteku");
-
-
-echo $Alpet->getName() . "<br>";
-echo $Alpet;
-
-
-$Filan->setName("Filani2");
-echo $Filan;
