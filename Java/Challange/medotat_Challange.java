3b400b60b1e18d11b85b9f88a99b4c1f14b4e918
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-06-04 01:51:18 +0200

    Create medotat_Challange.java

---
+++ Java/Challange/medotat_Challange.java
@@ -0,0 +1,70 @@
+package Challage;
+
+public class medotat_Challange {
+	public static void mosha(int mosha) {
+		System.out.println(mosha*365 + " dite i vjeter" );
+	}
+	
+	public static int moshaa(int moshaa) {
+		return moshaa * 365;
+	}
+	
+	public static void temp(double temp) {
+		System.out.println((5.0/9.0) * (temp - 32) + "F");
+	}
+	
+	public static double temp2(double temp) {
+		return (5.0/9.0) * (temp - 32);
+	}
+	
+	public static boolean plot(int num) {
+		if (num % 5 == 0  ) {
+			  return true;
+		} else {
+			return false;
+		}
+	
+	
+	}
+	public static void plot2(int num) {
+		if( num % 5 == 0 ) {
+			System.out.println(num +" eshte i plot pjestueshem");
+
+		} else {
+			System.out.println(num + " nuk eshte i plot pjestueshem");
+		}
+	}
+	
+	
+	public static double mesatarja(int [] vargu) {
+		int sum = 0;
+	
+		for(int i = 0; i < vargu.length; i++) {
+			sum += vargu[i];
+		}
+		double length = vargu.length;
+		double average = sum/length;
+		
+		return average;
+		
+	}
+
+	
+	public static void main(String[] args) {
+		
+		mosha(16);
+		System.out.println(moshaa(16)  + " dite i vjeter");
+		
+		temp(132.3);
+		System.out.println(temp2(132.3) + "F");
+		
+		plot2(13);
+		plot2(15);
+		System.out.println(plot(13)); 
+		System.out.println(plot(15));
+		
+		int[] vargu = {1,2,3,4,5};
+	
+		System.out.println(mesatarja(vargu));
+	}
+}
